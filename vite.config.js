05f47a4410183d8c287c7e791fc398500d74f59d
mjk-limon <jhmasterlimon11@gmail.com>
2024-08-19 15:46:09 +0600

    Init: Vite React, Inertia Dependency

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
 import { defineConfig, transformWithEsbuild } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  
+  base: '/',
+  plugins: [
+    react(),
+    laravel({
+      input: 'resources/js/index.jsx',
+      refresh: [
+        'resources/routes/**',
+        'routes/**',
+        'resources/views/**',
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
+    },
+  },
 });
