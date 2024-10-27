35df9713c7d77c748e0841222738f09238e8a8e9
mjk-limon <jahid.limon@prothomalo.com>
2024-10-27 12:04:21 +0600

    View: Frontend, Main blade init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="theme-color" content="#17c1e8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Epaper Prothom Alo</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet" />
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    @viteReactRefresh
+    @vite(['resources/js/index.jsx'])
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
